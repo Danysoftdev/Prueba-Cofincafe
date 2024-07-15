cbadf0f1949ba383c44e8dfa9cc89fca16111da4
Danysoftdev <danysoftdev@gmail.com>
2024-07-15 17:17:19 -0500

    Corregidos nombres variables y archivos

--- Backend/main.py
+++ Backend/main.py
@@ -1,41 +1,44 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
 import pandas as pd
-import io
 
 app = Flask(__name__)
 CORS(app)
 
-def process_data():
+def procesarDatos():
     
     archivoPersonal = pd.read_excel('Backend/Data/data1.xlsx')
     archivoFinanciero = pd.read_excel('Backend/Data/data2.xlsx')
 
-    archivoFusionado = pd.merge(archivoPersonal, archivoFinanciero, on='NIT')
+    archivoFusionado = pd.merge(archivoPersonal, archivoFinanciero, on='nit')
     
-    archivoFusionado['Nombre Completo'] = archivoFusionado['Nombre'] + ' ' + archivoFusionado['Apellido']
-    archivoFusionado['Total'] = archivoFusionado['Ingresos'] - archivoFusionado['Egresos']
+    archivoFusionado['total'] = archivoFusionado['ingresos'] - archivoFusionado['egresos']
 
     estadisticas = {
-        'Total Ingresos': archivoFusionado['Ingresos'].sum(),
-        'Total Egresos': archivoFusionado['Egresos'].sum(),
-        'Promedio Ingresos': archivoFusionado['Ingresos'].mean(),
-        'Promedio Egresos': archivoFusionado['Egresos'].mean(),
-        'Ingreso Máximo': archivoFusionado['Ingresos'].max(),
-        'Ingreso Mínimo': archivoFusionado['Ingresos'].min(),
-        'Egreso Máximo': archivoFusionado['Egresos'].max(),
-        'Egreso Mínimo': archivoFusionado['Egresos'].min()
+        'Total Ingresos': archivoFusionado['ingresos'].sum(),
+        'Total Egresos': archivoFusionado['egresos'].sum(),
+        'Promedio Ingresos': archivoFusionado['ingresos'].mean(),
+        'Promedio Egresos': archivoFusionado['egresos'].mean(),
+        'Ingreso Máximo': archivoFusionado['ingresos'].max(),
+        'Ingreso Mínimo': archivoFusionado['ingresos'].min(),
+        'Egreso Máximo': archivoFusionado['egresos'].max(),
+        'Egreso Mínimo': archivoFusionado['egresos'].min()
     }
     
     archivoEstadisticas = pd.DataFrame(list(estadisticas.items()), columns=['Descripcion', 'Valor'])
     
-    archivoFusionado.to_csv('Backend/Data/Results/base_data.csv', index=False)
-    archivoEstadisticas.to_excel('Backend/Data/Results/statistics.csv', index=False)
-
+    estadisticas = {k: float(v) for k, v in estadisticas.items()}
+    
+    archivoFusionado.to_csv('Backend/Data/Results/datos.csv', index=False)
+    archivoEstadisticas.to_csv('Backend/Data/Results/estadisticas.csv', index=False)
+    
     return archivoFusionado.to_dict('records'), estadisticas
 
 @app.route('/datos', methods=['GET'])
-def get_info():
-    datos, estadisticas = process_data()
-    return jsonify({'data': datos, 'stats': estadisticas})
+def obtenerInfo():
+    datos, estadisticas = procesarDatos()
+    return jsonify({'datos': datos, 'estadisticas': estadisticas})
+
+if __name__ == '__main__':
+    app.run(port=5000)  
 
