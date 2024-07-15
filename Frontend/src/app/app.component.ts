4112c17ccaf954770eb37eeac68c1511ec5ac584
Danysoftdev <danysoftdev@gmail.com>
2024-07-15 17:18:53 -0500

    Realizado frontend

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Frontend';
+  datosTabla = new MatTableDataSource<any>([]);
+  columnas: string[] = ['NIT', 'Nombre', 'Ingresos', 'Egresos', 'Total'];
+  estadisticas: any = {};
+  cargando = false;
+
+  constructor(private http: HttpClient) {}
+
+  cargarInfo() {
+    this.cargando = true;
+    this.http.get('http://localhost:5000/datos').subscribe(
+      (response: any) => {
+        this.datosTabla.data = response.datos;
+        this.estadisticas = response.estadisticas;
+        this.cargando = false;
+      },
+      error => {
+        console.error(error);
+        this.cargando = false;
+      }
+    );
+  }
+
 }
--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -3,6 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTableModule } from '@angular/material/table';
+import { MatCardModule } from '@angular/material/card';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @NgModule({
   declarations: [
@@ -10,7 +17,14 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    MatButtonModule,
+    MatTableModule,
+    MatCardModule,
+    MatToolbarModule,
+    MatProgressSpinnerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
